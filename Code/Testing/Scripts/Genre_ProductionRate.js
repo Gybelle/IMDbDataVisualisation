12742ac68f78cfaa90879198dedcea70ee77f6e3
Anaïs Ools <ools.anais@gmail.com>
2016-12-06 20:54:28 +0100

    LineChart genre version 1

--- Code/Testing/Scripts/Genre_ProductionRate.js
+++ Code/Testing/Scripts/Genre_ProductionRate.js
@@ -1,32 +1,84 @@
-function genreProductionRate(divID, w, h){
-  //Define color scale
-  var color =  d3.scale.category20()
+// Inspiration: http://bl.ocks.org/d3noob/d8be922a10cb0b148cd5
 
-  var svg = d3.select(divID)
-    .append('svg').attr('width', w).attr('height', h)
-    .append('g').attr('transform', 'translate(' + (w / 2) +  ',' + (h / 2) + ')');
+function genreProductionRate(divID, w, h) {
+    // Define color scale
+    var color = d3.scale.category20();
+    var margin = {top: 50, right: 50, bottom: 50, left: 50};
+    w = w - margin.left - margin.right;
+    h = h - margin.top - margin.bottom;
 
-  var parseTime = d3.time.format("%d %m %Y").parse;
+    var svg = d3.select(divID)
+            .append('svg')
+            .attr('width', w + margin.left + margin.right)
+            .attr('height', h + margin.top + margin.bottom)
+            .append('g')
+            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-  //Ranges:
-  var x = d3.time.scale().range([0, w]);
-  var y = d3.scale.linear().range([h, 0]);
+    var parseDate = d3.time.format("%Y").parse;
 
-  //Axes:
-  var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(1);
-  var yAxis = d3.svg.axis().scale(y).orient("left").ticks(1);
+    // Ranges:
+    var x = d3.time.scale().range([0, w]);
+    var y = d3.scale.linear().range([h, 0]);
 
-  //Define the line
-  var countLine = d3.svg.line()
-    .x(function(d) {
-      return x(d.year);
-    })
-    .y(function(d) {
-      return y(d.count);
-    })
+    // Axes:
+    var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(5);
+    var yAxis = d3.svg.axis().scale(y).orient("left").ticks(5);
 
+    //Define the line
+    var countLine = d3.svg.line()
+            .x(function (d) {
+                return x(d.year);
+            })
+            .y(function (d) {
+                return y(d.count);
+            });
 
+    // Get the data
+    d3.dsv(';')("genreTestData.csv", function (error, data) {
+        data.forEach(function (d) {
+            d.year = parseDate(d.year);
+            d.count = +d.count;
+            //console.log(d)
+        });
 
+        // Scale the range of the data
+        x.domain(d3.extent(data, function (d) {
+            return d.year;
+        }));
+        y.domain([0, d3.max(data, function (d) {
+                return d.count;
+            })]);
 
+        // Group entries
+        var groupedData = d3.nest()
+                .key(function (d) {
+                    return d.genre;
+                })
+                .entries(data);
 
+        // Add data lines to svg
+        groupedData.forEach(function (d) {
+            svg.append("path")
+                    .attr("class", "line")
+                    .attr("d", countLine(d.values))
+                    .style("stroke", color(d.key));
+        });
+
+        // Add x and y axis to svg
+        svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + h + ")")
+                .call(xAxis);
+        svg.append("g")
+                .attr("class", "y axis")
+                .call(yAxis);
+
+        // The Legend
+        var legend = d3.select(divID).append('div').attr("class", "legend");
+        groupedData.forEach(function (d) {
+            series = legend.append('div');
+            series.append('div').attr("class", "series-marker").style("background-color", color(d.key));
+            series.append('p').text(d.key);
+        });
+    });
 }
