ad5b4434dda0716976a5eadae595a2fe324e5417
Anaïs Ools <ools.anais@gmail.com>
2016-12-11 22:32:49 +0100

    BarChart added

--- Code/Testing/Scripts/Genre_ProductionMax.js
+++ Code/Testing/Scripts/Genre_ProductionMax.js
@@ -1,12 +1,13 @@
-// Inspiration: http://bl.ocks.org/d3noob/d8be922a10cb0b148cd5
+// Inspiration: https://bl.ocks.org/mbostock/3885304
+// Inspiration: http://bl.ocks.org/d3noob/8952219
 
-function genreProductionRate(divID, w, h, beginYearString, endYearString, genreFilter, countryFilter) {
+function genreProductionMax(divID, w, h, beginYearString, endYearString, genreFilter, countryFilter) {
+    console.log("Hoi");
+
     // Define color scale
-//    var color = d3.scale.category20();
     var color = d3.scale.ordinal()
             .domain(["Documentary", "Short", "Comedy", "Family", "Sport", "Action", "Animation", "Romance", "Drama", "Western", "News", "Horror", "History", "Crime", "Sci-Fi", "Biography", "Fantasy", "Music", "War", "Adventure", "Thriller", "Musical", "Mystery", "Adult", "Film-Noir", "Reality-TV", "Talk-Show", "Game-Show", "Erotica", "Experimental", "Commercial", "Sex", "Hardcore"])
             .range(["#6599C0", "#F0CC76", "#64BD91", "#F59A6E", "#AFD572", "#FF7F0E", "#E2D35C", "#D84E67", "#7073A0", "#58B16F", "#A2C5A5", "#C25D7F", "#FCD450", "#FF183C", "#2AB1CF", "#348B85", "#70C256", "#72CAFA", "#3A5DA1", "#4EA6AA", "#916589", "#C25D7F", "#4EE69B", "#D6AA51", "#DE6E48", "#AD6A8B", "#73539F", "#FF185D", "#57C27C", "#696C97", "#F7B6D2", "#DA707A", "#878787"]);
-
 
     var margin = {top: 50, right: 50, bottom: 50, left: 50};
     w = w - margin.left - margin.right;
@@ -24,21 +25,13 @@ function genreProductionRate(divID, w, h, beginYearString, endYearString, genreF
     var endYear = parseDate(endYearString);
 
     // Ranges:
-    var x = d3.time.scale().range([0, w]);
+    //var x = d3.time.scale().range([0, w]);
+    var x = d3.scale.ordinal().rangeRoundBands([0, w], 0);
     var y = d3.scale.linear().range([h, 0]);
 
     // Axes:
-    var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(10);
-    var yAxis = d3.svg.axis().scale(y).orient("left").ticks(10);
-
-    //Define the line
-    var countLine = d3.svg.line().interpolate("basis")
-            .x(function (d) {
-                return x(d.Year);
-            })
-            .y(function (d) {
-                return y(d.Count);
-            });
+    var xAxis = d3.svg.axis().scale(x).orient("bottom").tickFormat(d3.time.format("%Y")); // .ticks(10)
+    var yAxis = d3.svg.axis().scale(y).orient("left").ticks(1);
 
     // Get the data
     d3.dsv(';')("GenreYearCounty.csv", function (error, data) {
@@ -66,13 +59,13 @@ function genreProductionRate(divID, w, h, beginYearString, endYearString, genreF
             });
         }
 
-        // Group entries
+        // Group entries by (year, genre)
         var groupedData = d3.nest()
                 .key(function (d) {
-                    return d.Genre;
+                    return d.Year;
                 })
                 .key(function (d) {
-                    return d.Year;
+                    return d.Genre;
                 })
                 .rollup(function (d) {
                     return {
@@ -82,53 +75,57 @@ function genreProductionRate(divID, w, h, beginYearString, endYearString, genreF
                 })
                 .entries(data);
 
-        var maxCount = 0
-        groupedData.forEach(function (d) {
-            d.values.forEach(function (g) {
-                if (g.values.Count > maxCount) {
-                    maxCount = g.values.Count;
+        // Group entries again, by genre
+        var maxPerYear = [];
+        groupedData.forEach(function (d) { // for each year
+            var maxGenre = null;
+            var maxGenreCount = -1;
+            d.values.forEach(function (g) { // for each genre within year
+                if (g.values.Count > maxGenreCount) {
+                    maxGenre = g.key;
+                    maxGenreCount = g.values.Count;
                 }
+            });
+            maxPerYear.push({
+                Year: parseDate(d.key),
+                MaxGenre: maxGenre,
+                MaxGenreCount: maxGenreCount
             });
         });
 
         // Scale the range of the data
-        x.domain(d3.extent(data, function (d) {
-            return parseDate(d.Year);
-        }));
-        y.domain([0, maxCount]);
-
-        // Add data lines to svg
-        groupedData.forEach(function (d) {
-            // Aggregate data
-            var newValues = [];
-            d.values.forEach(function (g) {
-                newValues.push({
-                    Year: parseDate(g.key),
-                    Count: g.values.Count
-                });
-            });
-            // Draw data
-            svg.append("path")
-                    .attr("class", "line")
-                    .attr("d", countLine(newValues))
-                    .style("stroke", color(d.key));
-        });
+        //x.domain(d3.extent(data, function (d) { return parseDate(d.Year); }));
+        x.domain(maxPerYear.map(function (d) { return d.Year; }));
+        //x.domain([beginYear, endYear]);
+        y.domain([0, 1]);
 
         // Add x and y axis to svg
         svg.append("g")
                 .attr("class", "x axis")
                 .attr("transform", "translate(0," + h + ")")
                 .call(xAxis);
+        /* Uncomment to add y axis
         svg.append("g")
                 .attr("class", "y axis")
                 .call(yAxis);
+        */
 
-        // The Legend
-        var legend = d3.select(divID).append('div').attr("class", "legend");
-        groupedData.forEach(function (d) {
-            series = legend.append('div');
-            series.append('div').attr("class", "series-marker").style("background-color", color(d.key));
-            series.append('p').text(d.key);
-        });
+        // Add data to svg
+        svg.selectAll("bar")
+                .data(maxPerYear)
+                .enter().append("rect")
+                .style("fill", function (d) {
+                    return color(d.MaxGenre);
+                })
+                .attr("x", function (d) {
+                    return x(d.Year);
+                })
+                .attr("width", x.rangeBand())
+                .attr("y", function (d) {
+                    return y(1);
+                })
+                .attr("height", function (d) {
+                    return h - y(1);
+                });
     });
 }
