03dc15e0dd14dda72c97a530edf6bdfb5dfcaae6
Michelle Gybels <michelle.gybels@gmail.com>
2016-12-22 13:49:58 +0100

    Query for gathering flow data part 2: Defining bucket names

--- Preprocessing/Queries/QueryGenerateFlowData.py
+++ Preprocessing/Queries/QueryGenerateFlowData.py
@@ -6,16 +6,33 @@ Author: Michelle Gybels
 
 import csv
 from recordclass import recordclass
+import chunk
 import math
 import re
 
 movieList = []      # (id, title, runtime, filmingDays, budget, gross, rating)
-Range = recordclass('Range', 'min max')
-rangeRuntime = Range(-1, -1)
-rangeFilmingDays = Range(-1, -1)
-rangeBudget = Range(-1, -1)
-rangeGross = Range(-1, -1)
-rangeScore = Range(0, 10)
+
+RangeLimits = recordclass('RangeLimits', 'min max')
+rangeLimitRuntime = RangeLimits(-1, -1)
+rangeLimitFilmingDays = RangeLimits(-1, -1)
+rangeLimitBudget = RangeLimits(-1, -1)
+rangeLimitGross = RangeLimits(-1, -1)
+rangeLimitScore = RangeLimits(0, 10)
+
+Ranges = recordclass('Ranges', 'r1 r2 r3 r4 r5 r6 r7 r8 r9 r10 r11')
+rangeScore = Ranges(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+rangeRuntime = Ranges(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+rangeFilmingDays = Ranges(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+rangeBudget = Ranges(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+rangeGross = Ranges(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+RangeNames = recordclass('RangeNames', 't1 t2 t3 t4 t5 t6 t7 t8 t9 t10 t11')
+rangeNamesScore = RangeNames("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
+rangeNamesRuntime = RangeNames("", "", "", "", "", "", "", "", "", "", "")
+rangeNamesFilmingDays = RangeNames("", "", "", "", "", "", "", "", "", "", "")
+rangeNamesBudget = RangeNames("", "", "", "", "", "", "", "", "", "", "")
+rangeNamesGross = RangeNames("", "", "", "", "", "", "", "", "", "", "")
+
 
 #######################################################################################################################
 #                    PART ONE: PROCESSING MOVIES.CSV AND CALCULATING RANGES MIN AND MAX                               #
@@ -32,14 +49,14 @@ def process(file):
     printMessage_ProcessingEnded()
 
 def fixRanges():
-    if rangeRuntime.min < 0:
-        rangeRuntime.min = 1
-    if rangeFilmingDays.min < 0:
-        rangeFilmingDays.min = 1
-    if rangeBudget.min < 0:
-        rangeBudget.min = 1
-    if rangeGross.min < 0:
-        rangeGross.min = 1
+    if rangeLimitRuntime.min < 0:
+        rangeLimitRuntime.min = 1
+    if rangeLimitFilmingDays.min < 0:
+        rangeLimitFilmingDays.min = 1
+    if rangeLimitBudget.min < 0:
+        rangeLimitBudget.min = 1
+    if rangeLimitGross.min < 0:
+        rangeLimitGross.min = 1
 
 def processLine(line):
     # Get data
@@ -57,7 +74,6 @@ def processLine(line):
         try:
             rating = float(rating)
         except:
-            print("----rating")
             print(line)
     else:
         rating = None
@@ -173,10 +189,10 @@ def updateMinMax(record):           # (id, title, runtime, filmingDays, budget, 
     budget = record[4]
     gross = record[5]
 
-    checkMinMax(rangeRuntime, runtime)
-    checkMinMax(rangeFilmingDays, filmingDays)
-    checkMinMax(rangeBudget, budget)
-    checkMinMax(rangeGross, gross)
+    checkMinMax(rangeLimitRuntime, runtime)
+    checkMinMax(rangeLimitFilmingDays, filmingDays)
+    checkMinMax(rangeLimitBudget, budget)
+    checkMinMax(rangeLimitGross, gross)
 
 def checkMinMax(range, value):
     min = range.min
@@ -195,21 +211,100 @@ def checkMinMax(range, value):
 def printMessage_ProcessingEnded():
     print("===========================================")
     print("PROCESSING FILE FINISHED")
+    print("Runtime: %s" % rangeLimitRuntime)
+    print("Filming days: %s" % rangeLimitFilmingDays)
+    print("Budget: %s" % rangeLimitBudget)
+    print("Gross: %s" % rangeLimitGross)
+    print("===========================================")
+
+#######################################################################################################################
+#                    PART TWO: CREATING BUCKETS FOR EACH RANGE TO DISTRIBUTE THE DATA IN                              #
+#######################################################################################################################
+def calculateRanges(rangeCount):
+    calculateRangesFor(rangeRuntime, rangeCount, rangeLimitRuntime.max)
+    calculateRangesFor(rangeFilmingDays, rangeCount, rangeLimitFilmingDays.max)
+    calculateRangesFor(rangeBudget, rangeCount, rangeLimitBudget.max)
+    calculateRangesFor(rangeGross, rangeCount, rangeLimitGross.max)
+    printMessage_CalculatingRangesEnded()
+
+    generateRangeNames()
+    printMessage_GeneratingNamesRangesEnded()
+
+def calculateRangesFor(ranges, count, max):
+    step = max/11
+    if step < 1.0:
+        stepSize = 1
+    else:
+        stepSize = math.floor(step)
+
+    ranges.r1 = 0 + stepSize
+    ranges.r2 = ranges.r1 + stepSize
+    ranges.r3 = ranges.r2 + stepSize
+    ranges.r4 = ranges.r3 + stepSize
+    ranges.r5 = ranges.r4 + stepSize
+    ranges.r6 = ranges.r5 + stepSize
+    ranges.r7 = ranges.r6 + stepSize
+    ranges.r8 = ranges.r7 + stepSize
+    ranges.r9 = ranges.r8 + stepSize
+    ranges.r10 = ranges.r9 + stepSize
+    ranges.r11 = ranges.r10 + stepSize
+
+def generateRangeNames():
+    generateNamesForRange(rangeRuntime, rangeNamesRuntime)
+    generateNamesForRange(rangeFilmingDays, rangeNamesFilmingDays)
+    generateNamesForRange(rangeBudget, rangeNamesBudget)
+    generateNamesForRange(rangeGross, rangeNamesGross)
+
+def generateNamesForRange(ranges, rangeNames):
+    rangeNames.t1 = "[1 - " + numberToString(ranges.r1) + "]"
+    rangeNames.t2 = "[" + numberToString(ranges.r1+1) + " - " + numberToString(ranges.r2) + "]"
+    rangeNames.t3 = "[" + numberToString(ranges.r2+1) + " - " + numberToString(ranges.r3) + "]"
+    rangeNames.t4 = "[" + numberToString(ranges.r3+1) + " - " + numberToString(ranges.r4) + "]"
+    rangeNames.t5 = "[" + numberToString(ranges.r4+1) + " - " + numberToString(ranges.r5) + "]"
+    rangeNames.t6 = "[" + numberToString(ranges.r5+1) + " - " + numberToString(ranges.r6) + "]"
+    rangeNames.t7 = "[" + numberToString(ranges.r6+1) + " - " + numberToString(ranges.r7) + "]"
+    rangeNames.t8 = "[" + numberToString(ranges.r7+1) + " - " + numberToString(ranges.r8) + "]"
+    rangeNames.t9 = "[" + numberToString(ranges.r8+1) + " - " + numberToString(ranges.r9) + "]"
+    rangeNames.t10 = "[" + numberToString(ranges.r9+1) + " - " + numberToString(ranges.r10) + "]"
+    rangeNames.t11 = "[" + numberToString(ranges.r10+1) + " - " + numberToString(ranges.r11) + "]"
+
+def numberToString(number):
+    return '{0:,}'.format(number)
+
+def printMessage_CalculatingRangesEnded():
+    print("===========================================")
+    print("CALCULATING RANGES FINISHED")
     print("Runtime: %s" % rangeRuntime)
     print("Filming days: %s" % rangeFilmingDays)
     print("Budget: %s" % rangeBudget)
     print("Gross: %s" % rangeGross)
     print("===========================================")
 
-#######################################################################################################################
-#                    PART TWO: CREATING BUCKETS FOR EACH RANGE TO DISTRIBUTE THE DATA IN                              #
-#######################################################################################################################
+def printMessage_GeneratingNamesRangesEnded():
+    print("===========================================")
+    print("GENERATING NAMES FOR RANGES FINISHED")
+    print("Runtime: %s" % rangeNamesRuntime)
+    print("Filming days: %s" % rangeNamesFilmingDays)
+    print("Budget: %s" % rangeNamesBudget)
+    print("Gross: %s" % rangeNamesGross)
+    print("===========================================")
+
 
 #######################################################################################################################
-#                            PART THREE: DISTRIBUTING DATA ACCROSS THE RANGES                                         #
+#                            PART THREE: DISTRIBUTING DATA ACROSS THE RANGES                                         #
+#######################################################################################################################
+
+
+#######################################################################################################################
+#                                               MAIN SCRIPT                                                           #
 #######################################################################################################################
 
 
 movieFile = open("../../Data/movies.csv", "r", encoding="utf8", errors="ignore")
 process(movieFile)
 movieFile.close()
+
+calculateRanges(len(rangeRuntime))
+
+
+
