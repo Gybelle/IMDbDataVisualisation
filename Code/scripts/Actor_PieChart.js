73f42105b28bf583a39a25eca70e37c852a341c1
Anaïs Ools <ools.anais@gmail.com>
2016-12-30 15:34:44 +0100

    Pie chart legend updated

--- Code/scripts/Actor_PieChart.js
+++ Code/scripts/Actor_PieChart.js
@@ -1,27 +1,29 @@
-var pie = null;
-var arc = null;
-var path = null;
-var color = null;
-var svg = null;
-var text = null;
-var tooltip = null;
+var unfilteredData = null;
+var filteredData = null;
+var divID = null;
 
-function createActorPieChart(divID, data) {
+function createActorPieChart(divId, data) {
+    divID = divId;
     if (data == null) {
         data = [
             {language: 'No data', count: 0}
         ];
     }
+    unfilteredData = data;
+    filteredData = null;
+    drawChart(unfilteredData);
+}
 
+function drawChart(data) {
     var width = widthSmallLargeChart;
     var height = heightSmallRow;
     var radius = Math.min(width, height) / 2;
-    var donutWidth = 65;
+    var donutWidth = 40;
 
-    color = d3.scale.ordinal()
-            .range(["#6599C0", "#F0CC76", "#64BD91", "#F59A6E", "#AFD572", "#FF7F0E", "#E2D35C", "#D84E67", "#7073A0", "#58B16F", "#A2C5A5", "#C25D7F", "#FCD450", "#FF183C", "#2AB1CF", "#348B85", "#70C256", "#72CAFA", "#3A5DA1", "#4EA6AA", "#916589", "#C25D7F", "#4EE69B", "#D6AA51", "#DE6E48", "#AD6A8B", "#73539F", "#FF185D", "#57C27C", "#696C97", "#F7B6D2", "#DA707A", "#878787"]);
+    var color = d3.scale.ordinal()
+            .range(["FF7F0E", "#6599C0", "#F0CC76", "#64BD91", "#F59A6E", "#AFD572", "#E2D35C", "#D84E67", "#7073A0", "#58B16F", "#A2C5A5", "#C25D7F", "#FCD450", "#FF183C", "#2AB1CF", "#348B85", "#70C256", "#72CAFA", "#3A5DA1", "#4EA6AA", "#916589", "#C25D7F", "#4EE69B", "#D6AA51", "#DE6E48", "#AD6A8B", "#73539F", "#FF185D", "#57C27C", "#696C97", "#F7B6D2", "#DA707A", "#878787"]);
 
-    arc = d3.svg.arc()
+    var arc = d3.svg.arc()
             .innerRadius(radius - donutWidth)
             .outerRadius(radius);
 
@@ -29,13 +31,13 @@ function createActorPieChart(divID, data) {
             .outerRadius(radius - 20)
             .innerRadius(radius - 20);
 
-    pie = d3.layout.pie()
+    var pie = d3.layout.pie()
             .value(function (d) {
                 return d.count;
             });
 
     d3.select("svg").remove();
-    svg = d3.select(divID).append("svg")
+    var svg = d3.select(divID).append("svg")
             .attr("width", width)
             .attr("height", height)
             .append("g")
@@ -45,29 +47,32 @@ function createActorPieChart(divID, data) {
             .data(pie(data)).enter()
             .append("g").attr("class", "arc");
 
-    path = g.append("path")
+    var path = g.append("path")
             .attr("d", arc)
             .style("fill", function (d) {
                 return color(d.data.language);
             })
             .attr("class", "slice");
 
-    text = g.append("text")
+    var text = g.append("text")
             .attr("transform", function (d) {
                 return "translate(" + labelArc.centroid(d) + ")";
             })
             .attr("dy", ".35em")
             .text(function (d) {
-                return d.data.count;
+                if (d.data.count != 0) {
+                    return d.data.count;
+                }
+                return;
             });
 
-    addMouseOver(data);
+    addMouseOver(svg);
 
     /* The Legend */
-    //createLegend();
+    createLegend(data, svg, color);
 }
 
-function addMouseOver(data) {
+function addMouseOver(svg) {
     var slice = svg.selectAll("path.slice");
     slice.on("mousemove", function (d) {
         if (d.data.count == 1) {
@@ -81,16 +86,17 @@ function addMouseOver(data) {
     });
 }
 
-function createLegend() {
-    var legendRectSize = 8;
-    var legendSpacing = 3;
+function createLegend(data, svg, color) {
+    var legendRectSize = 12;
+    var legendSpacing = 4;
+    var topDataColors = getTopData(data);
 
     var legend = svg.selectAll('.legend').remove()
-            .data(color.domain()).enter()
+            .data(topDataColors).enter()
             .append('g').attr('class', 'legend')
             .attr('transform', function (d, i) {
                 var height = legendRectSize + legendSpacing;
-                var offset = height * color.domain().length / 2;
+                var offset = height * topDataColors.length / 2;
                 var horz = -2 * legendRectSize;
                 var vert = i * height - offset;
                 return 'translate(' + horz + ',' + vert + ')';
@@ -110,32 +116,17 @@ function createLegend() {
             });
 }
 
-function updateActorPieChart(newData) {
-    console.log(newData);
-
-    var tmpData = [
-        {language: 'English', count: 20},
-        {language: 'Swedish', count: 15},
-        {language: 'Something', count: 2},
-        {language: 'Something else', count: 2}
-    ];
-
-//    pie.value(function (d) {
-//        return d.count;
-//    });
-//    path = path.data(pie);
-//    path.attr("d", arc);
-
-
-    path = path.data(pie(tmpData));
-    path.attr("d", arc)
-            .style("fill", function (d) {
-                return color(d.data.language);
-            });
-    ;
-    text = text.text(function (d) {
-        return d.data.count;
+function getTopData(data) {
+    var topSize = 5;
+    if (data.length > topSize) {
+        data = data.sort(function (x, y) {
+            return d3.descending(x.count, y.count);
+        });
+        data = data.slice(0, topSize);
+    }
+    var result = [];
+    data.forEach(function (element) {
+        result.push(element.language);
     });
-    //path.transition().duration(750).attrTween("d", arcTween);
-    createLegend();
+    return result;
 }
